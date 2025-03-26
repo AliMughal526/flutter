19e3973ba79b6b60ecf722fabf973bcea672dfad
ayesha526 <alimughalnns222@gmail.com>
2025-03-26 03:52:23 -0700

    Add files via upload

---
+++ image_data.dart
@@ -0,0 +1,16 @@
+// lib/image_data.dart
+
+// List of internet image URLs
+final List<String> internetImages = [
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkxF-AqNQPfZ8lRjtyScvvFgPMTMDB26-DbWyHZTWW2qcm2B2AVKTr6FUSpboaCnnbYhU&usqp=CAU',
+  'https://thumbs.dreamstime.com/b/stony-weir-small-mountain-river-stream-flowing-over-square-blocks-makes-milky-water-sandstone-55588245.jpg',
+  'https://cdn.pixabay.com/video/2024/08/30/228847_tiny.jpg',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcKulfRi6HUr5Xg_6lXkOPuGzO_0zQjKN-eA&s',
+];
+
+// List of local image asset paths
+final List<String> localImages = [
+  'assets/pic1.jpeg',
+  'assets/pic3.jpeg',
+  'assets/pic4.jpeg',
+];
